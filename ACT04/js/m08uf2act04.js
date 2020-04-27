c635f3f0e269cb693dabb6b967047efbd6675b78
xavi <xavi@Mac-mini-de-monica.local>
2020-04-27 17:10:21 +0200

    canvis 20402020

--- ACT04/js/m08uf2act04.js
+++ ACT04/js/m08uf2act04.js
@@ -1,7 +1,7 @@
 //Variables
 //David Vallejo Arcos
 
-document.write("<br/>");
+document.write("aa<br/>");
 var pri = 10;
 var seg  = 20;
 var terc = 30;
@@ -9,7 +9,7 @@ var qua  = 40;
 
 //Tasca 1. Comparar totes les variables, i digues quina és més gran de totes
 var menor = Math.min(pri, seg, terc, qua);
-document.write("La següent variable és més petita", "<br/><br/><br/>", menor);
+document.write("La següent variable és més petita <br/><br/><br/>" + menor + "<br/>");
 var trenta   = 30;
 var vint     = 20;
 var deu      = 10;
@@ -18,14 +18,14 @@ var quaranta = 40;
 
 //Tasca 2. Comparar totes les variables, i digues quina és  més petita de totes
 var major = Math.max(trenta, vint, deu, quaranta);
-document.write("La següent variable és més granLa següent variable és més petita", "<br/><br/><br/>", major);
+document.write("La següent variable és més granLa següent variable és més petita <br/><br/><br/>" + major  + "<br/>");
 var un      = 10
 var dos     = 20;
 var tres    = 30;
 var quatre  = 40;
 
 //Tasca 3. Comparar totes les variables, i digues quines són iguals
-if (un !== dos && dos !-- tres && tres !-- quatre);
-{
-  document.write("Les següents variables no són iguals");
-}
+//if (un !== dos && dos !-- tres && tres !-- quatre);
+//{
+//  document.write("Les següents variables no són iguals");
+//}
