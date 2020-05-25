65d00d9734a90401fa07e565479f5adddbcc1a9b
dvallejoa <60621697+dvallejoa@users.noreply.github.com>
2020-05-25 06:08:22 +0200

    Add files via upload

---
+++ ACT05/js/activitat05.js
@@ -0,0 +1,37 @@
+//Variables de numeros
+var numnueve = 9;
+var numcinco = 5;
+var numuno = 1;
+
+//Variables de resultados
+var resultadoa = 1;
+var resultadob = 1;
+var resultadoc = 1;
+
+//Tasca 1. Imprimeix la taula de multiplicar del 9, utilitzant un bucle.
+document.write("<h2> Activitat 1</h2>")
+for (vallejo = 0; resultadoa <= 11; vallejo++) {
+  document.write(numnueve + "x" +vallejo+ "=" +numnueve*vallejo+"</br>");
+  resultadoa++
+  document.write("</br>")
+}
+
+//Tasca 2. Imprimeix la taula de multiplicar del 5 a la inversa, utilitzant un bucle.
+document.write("<h2> Activitat 2</h2>")
+for (vallejo = 10; resultadob <= 11; vallejo--) {
+    document.write(numcinco + "x" +vallejo+ "=" +numcinco*vallejo+ "</br>");
+    resultadob++
+    document.write("</br>")
+}
+
+//Tasca 3. Imprimeix totes les taules de multiplicar, utilitzant dos o més bucles.
+document.write("<h2> Activitat 3</h2>")
+for (vallejo = 0; resultadoc <= 101; vallejo++) {
+  document.write(numuno + "x" +vallejo+ "=" +numuno*vallejo+ "</br>");
+  resultadoc++
+  if (vallejo == 10) {
+    document.write("</br>");
+    vallejo = 0;
+    numuno++;
+  }
+}
